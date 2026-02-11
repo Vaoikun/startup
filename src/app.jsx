44d458d08601c82eb9a93c8208ec27c540de6e4b
Vaoikun <furious_sesame@icloud.com>
2026-02-10 22:27:02 -0700

    new custom fonts

--- src/app.jsx
+++ src/app.jsx
@@ -42,10 +42,10 @@ export default function App() {
             <hr />
         </header>
 
-        <main>App components go here</main>
+        <main className="nfs-tagline">Need For Speed...</main>
 
         <Routes>
-            <Route path='/' element={<Login />} exact />
+            <Route path='/' element={<Login />} />
             <Route path='/account' element={<Account />} />
             <Route path='/schedule' element={<Schedule />} />
             <Route path='/about' element={<About />} />
