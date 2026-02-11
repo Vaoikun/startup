a3cfae273e67ff1d7fa02e18933a572c02939015
Vaoikun <furious_sesame@icloud.com>
2026-02-10 22:07:34 -0700

    login.jsx updated

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+        <main>
+        <h2>Welcome to Kai Tuning</h2>
+        <p>Welcome to the Kai Tuning project! This project is dedicated to providing resources and information about Kai Tuning.</p>
+        <h2>About Kai Tuning</h2>
+        <p>Kai Tuning is a method of optimizing performance and efficiency in various systems. It involves fine-tuning parameters to achieve the best possible results.</p>
+        <form method="get" action="schedule.html">
+            <div>
+            <span>@</span>
+            <input type="text" placeholder="your@email.com" />
+            </div>
+            <div>
+            <span>🔒</span>
+            <input type="password" placeholder="password" />
+            </div>
+            <button type="submit">Login</button>
+            <button type="submit">Create</button>
+        </form>
+        </main>
   );
 }
