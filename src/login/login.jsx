a946a6e0b4ae2415ae234026012721359ca04d35
Vaoikun <furious_sesame@icloud.com>
2026-02-17 22:55:32 -0700

    new design overall

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -7,13 +7,13 @@ import { AuthState } from './authState';
 export function Login({ userName, authState, onAuthChange}) {
   return (
         <main classname='container-fluid bg-secondary text-center'>
+            <h2>Welcome to Kai Tuning</h2>
+            <p>Welcome to the Kai Tuning project! This project is dedicated to providing resources and information about Kai Tuning.</p> 
+            <h2>About Kai Tuning</h2> 
+            <p>Kai Tuning is a method of optimizing performance and efficiency in various systems. It involves fine-tuning parameters to achieve the best possible results.</p>
           <div>
             {authState !== AuthState.Unknown && 
-            <h2>Welcome to Kai Tuning</h2> &&
-            <p>Welcome to the Kai Tuning project! This project is dedicated to providing resources and information about Kai Tuning.</p> &&
-            <h2>About Kai Tuning</h2> &&
-            <p>Kai Tuning is a method of optimizing performance and efficiency in various systems. It involves fine-tuning parameters to achieve the best possible results.</p> &&
-            <p>Login or create a new accout to make an appointment!</p>
+            <p>Login or create an accout to make an appointment!</p>
             }
             {authState === AuthState.Authenticated && (
               <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)}/>
