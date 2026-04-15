d4c9ab5a4538356e41c5af88b459212142e03290
Vaoikun <furious_sesame@icloud.com>
2026-04-15 17:42:00 -0600

    user fucntions updated

--- service/database.js
+++ service/database.js
@@ -3,9 +3,10 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
-const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
+const db = client.db('startup');
+const userCollection = db.collection('users');
+const vehicleCollection = db.collection('vehicles');
+const appointmentCollection = db.collection('appointments');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -17,7 +18,7 @@ const scoreCollection = db.collection('score');
     process.exit(1);
   }
 })();
-
+// User functions
 function getUser(email) {
   return userCollection.findOne({ email: email });
 }
@@ -34,30 +35,18 @@ async function updateUser(user) {
   await userCollection.updateOne({ email: user.email }, { $set: user });
 }
 
-async function updateUserRemoveAuth(user) {
-  await userCollection.updateOne({ email: user.email }, { $unset: { token: 1 } });
+async function removeUserToken(email) {
+  await userCollection.updateOne({ email: email }, { $unset: { token: "" } });
 }
 
-async function addScore(score) {
-  return scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
-}
+// Vehicle functions
 
 module.exports = {
   getUser,
   getUserByToken,
   addUser,
   updateUser,
-  updateUserRemoveAuth,
+  removeUserToken,
   addScore,
   getHighScores,
 };
