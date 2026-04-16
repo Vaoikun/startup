2be7059e9cbf79b1a7ccd097ff1a28a5f02bf1a0
Vaoikun <furious_sesame@icloud.com>
2026-04-15 23:36:24 -0600

    pleaaaase

--- service/database.js
+++ service/database.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const config = require('./dbConfig.json');
 
-const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const url = `mongodb+srv://${config.username}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
 const userCollection = db.collection('users');
